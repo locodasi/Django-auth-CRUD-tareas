f223e38ce4823b699c33b551f901dba4b09668e3
Lucas <fradkinlucas@hotmail.com>
2023-11-09 18:16:04 -0300

    Verificar que se cargue en local

--- DjangoCRUD/settings.py
+++ DjangoCRUD/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -81,13 +81,21 @@ WSGI_APPLICATION = 'DjangoCRUD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-   'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
+#Si esta en debig usa como DB SQLite, si no usa el Postgrade
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+            'default': dj_database_url.config(# Feel free to alter this value to suit your needs
+                default='postgresql://postgres:postgres@localhost:5432/mysite', 
+                conn_max_age=600)
+    }
+
 
 
 # Password validation
